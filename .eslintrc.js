d4d239aa23fda39c8fa28a9c6dd4291ce3c84c46
whyareyou <bionixxxd5@gmail.com>
2023-07-27 17:59:44 +0300

    add redux toolkit, add json-server

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,7 +57,8 @@ module.exports = {
       },
     ],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error"
+    "react-hooks/exhaustive-deps": "error",
+    "no-param-reassign": "off",
   },
   globals: {
     __IS_DEV__: true,
